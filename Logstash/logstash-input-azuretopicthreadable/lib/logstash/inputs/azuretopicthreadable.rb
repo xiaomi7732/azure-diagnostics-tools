4a0999ceb00bff3d8b0d7197a768250a2adddd6d
Benedict Law <blaw@fleetcarma.com>
2017-08-04 09:34:06 -0400

    Added SAS key support. Improve performance when queue is not empty.

--- Logstash/logstash-input-azuretopicthreadable/lib/logstash/inputs/azuretopicthreadable.rb
+++ Logstash/logstash-input-azuretopicthreadable/lib/logstash/inputs/azuretopicthreadable.rb
@@ -11,33 +11,44 @@ class LogStash::Inputs::Azuretopicthreadable < LogStash::Inputs::Base
 
   config_name "azuretopicthreadable"
   milestone 1
-  
+
   default :codec, "json" # default json codec
-  
+
   config :namespace, :validate => :string
+  config :access_key_name, :validate => :string, :required => false
   config :access_key, :validate => :string
   config :subscription, :validate => :string
   config :topic, :validate => :string
   config :deliverycount, :validate => :number, :default => 10
   config :threads, :validate => :number, :default => 1
   config :thread_sleep_time, :validate => :number, :default => 1.0/50.0
-  
+
   def initialize(*args)
   super(*args)
   end # def initialize
-  
+
   public
   def register
     # Configure credentials
     Azure.configure do |config|
       config.sb_namespace = @namespace
       config.sb_access_key = @access_key
+      config.sb_sas_key_name = @access_key_name
+      config.sb_sas_key = @access_key
     end
   end # def register
-  
+
   def process(output_queue, pid)
     # Get a new instance of a service
-    azure_service_bus = Azure::ServiceBus::ServiceBusService.new
+    if @access_key_name
+        # SAS key used
+        signer = Azure::ServiceBus::Auth::SharedAccessSigner.new
+        sb_host = "https://#{Azure.sb_namespace}.servicebus.windows.net"
+        azure_service_bus = Azure::ServiceBus::ServiceBusService.new(sb_host, { signer: signer})
+    else
+        # ACS key
+        azure_service_bus = Azure::ServiceBus::ServiceBusService.new
+    end
     while !stop?
       begin
         # check if we have a message in the subscription
@@ -47,9 +58,12 @@ class LogStash::Inputs::Azuretopicthreadable < LogStash::Inputs::Base
           codec.decode(message.body) do |event|
           decorate(event)
           output_queue << event
-        end # codec.decode
-        # delete the message after reading it
-        azure_service_bus.delete_subscription_message(message)
+          # delete the message after reading it
+          azure_service_bus.delete_subscription_message(message)
+        end
+        if !message
+          sleep(@thread_sleep_time) #topic is probably empty. sleep. 
+        end
       end
       rescue LogStash::ShutdownSignal => e
         raise e
@@ -59,10 +73,9 @@ class LogStash::Inputs::Azuretopicthreadable < LogStash::Inputs::Base
           azure_service_bus.delete_subscription_message(message)
         end
       end
-      sleep(@thread_sleep_time)
     end
   end # def process
-  
+
   public
   def run(output_queue)
     threads = []
@@ -71,7 +84,7 @@ class LogStash::Inputs::Azuretopicthreadable < LogStash::Inputs::Base
       end
     threads.each { |thr| thr.join }
   end # def run
- 
+
   public
   def teardown
   end # def teardown
