51305f04474018a39d9ffa162023178e2b81fa68
Claudiu Guiman <clguiman@microsoft.com>
2017-09-08 11:13:17 -0700

    Fix logstash-input-azureeventhub (#90)
    * Add extra logging
    
    * Filter from last event offset on connection reset
    
    * Handle receive errors and recover #45 #66
    
    * update logstash-input-azureeventhub version to 0.9.8
    
    * small fix

--- Logstash/logstash-input-azureeventhub/lib/logstash/inputs/azureeventhub.rb
+++ Logstash/logstash-input-azureeventhub/lib/logstash/inputs/azureeventhub.rb
@@ -40,43 +40,74 @@ class LogStash::Inputs::Azureeventhub < LogStash::Inputs::Base
   end # def register
 
   def get_pay_load(message)
-    return nil if not message
+    return nil, nil if not message
+    annotationMap = nil
+    body = nil
     message.getPayload().each do |section|
-      if section.java_kind_of? org::apache::qpid::amqp_1_0::type::messaging::Data or section.java_kind_of? org::apache::qpid::amqp_1_0::type::messaging::AmqpValue
-        return section.getValue().to_s.gsub("\\x5c", "\\")
+      if section.java_kind_of? org::apache::qpid::amqp_1_0::type::messaging::MessageAnnotations
+        annotationMap = section.getValue()
+      elsif section.java_kind_of? org::apache::qpid::amqp_1_0::type::messaging::Data or section.java_kind_of? org::apache::qpid::amqp_1_0::type::messaging::AmqpValue
+          body = section.getValue().to_s.gsub("\\x5c", "\\")
       end
     end
-    return nil
+    return body, annotationMap
   end
   
-  def process(output_queue, receiver, partition)
+  def process(output_queue, receiver, partition, last_event_offset)
     while !stop?
       begin
         msg = receiver.receive(10)
         if msg
-          codec.decode(get_pay_load(msg)) do |event|
+          body, annotationMap = get_pay_load(msg)
+          last_event_offset = annotationMap.get(org::apache::qpid::amqp_1_0::type::Symbol.valueOf("x-opt-offset")) unless annotationMap.nil?
+          @logger.debug("[#{partition.to_s.rjust(2,"0")}] Event: #{body[0..50] unless body.nil?}... " <<
+            "Offset: #{annotationMap.get(org::apache::qpid::amqp_1_0::type::Symbol.valueOf("x-opt-offset")) unless annotationMap.nil? } " <<
+            "Time: #{annotationMap.get(org::apache::qpid::amqp_1_0::type::Symbol.valueOf("x-opt-enqueued-time")).to_s unless annotationMap.nil? } " <<
+            "Sequence: #{annotationMap.get(org::apache::qpid::amqp_1_0::type::Symbol.valueOf("x-opt-sequence-number")).to_s unless annotationMap.nil? }")
+
+          codec.decode(body) do |event|
             decorate(event)
             output_queue << event
           end
           receiver.acknowledge(msg)
         else
-          @logger.debug("  " + partition.to_s.rjust(2,"0") + " --- " + "No message")
-          sleep(@thread_wait_sec)
+          error = receiver.getError()
+          if error
+            @logger.debug("[#{partition.to_s.rjust(2,"0")}] Receive error: #{error.to_s}")
+            receiver.close()
+            return last_event_offset
+          else
+            @logger.debug("[#{partition.to_s.rjust(2,"0")}] No message")
+            sleep(@thread_wait_sec)
+          end
         end
-      rescue LogStash::ShutdownSignal => e
-        raise e
-      rescue org::apache::qpid::amqp_1_0::client::ConnectionErrorException => e
-        raise e
-      rescue => e
-        @logger.error("  " + partition.to_s.rjust(2,"0") + " --- " + "Oh My, An error occurred.", :exception => e)
       end
-    end # process
+    end
+  rescue LogStash::ShutdownSignal => e
+    @logger.debug("[#{partition.to_s.rjust(2,"0")}] ShutdownSignal received")
+    raise e
+  rescue org::apache::qpid::amqp_1_0::client::ConnectionErrorException => e
+    @logger.error("[#{partition.to_s.rjust(2,"0")}] ConnectionErrorException \nError:#{e}:\nTrace:\n#{e.backtrace}", :exception => e)
+    raise e
+  rescue => e
+    @logger.error("[#{partition.to_s.rjust(2,"0")}] Oh My, An error occurred. \nError:#{e}:\nTrace:\n#{e.backtrace}", :exception => e)
+    raise e
+  ensure
+    return last_event_offset
   end # process
   
   def process_partition(output_queue, partition)
+    last_event_offset = nil
     while !stop?
       begin
-        filter = SelectorFilter.new "amqp.annotation.x-opt-enqueuedtimeutc > '" + @time_since_epoch_millis.to_s + "'"
+        filter = nil
+        if !last_event_offset.nil?
+          @logger.debug("[#{partition.to_s.rjust(2,"0")}] Offset filter: x-opt-offset > #{last_event_offset}")
+          filter = SelectorFilter.new "amqp.annotation.x-opt-offset > '" + last_event_offset + "'"
+        else
+          @logger.debug("[#{partition.to_s.rjust(2,"0")}] Timestamp filter: x-opt-enqueuedtimeutc > #{@time_since_epoch_millis}")
+          filter = SelectorFilter.new "amqp.annotation.x-opt-enqueuedtimeutc > '" + @time_since_epoch_millis.to_s + "'"
+        end
         filters = { org::apache::qpid::amqp_1_0::type::Symbol.valueOf("apache.org:selector-filter:string") => filter }
 
         host = @namespace + "." + @domain
@@ -85,18 +116,19 @@ class LogStash::Inputs::Azureeventhub < LogStash::Inputs::Base
         receiveSession = connection.createSession()
         receiver = receiveSession.createReceiver(@eventhub + "/ConsumerGroups/" + @consumer_group + "/Partitions/" + partition.to_s, org::apache::qpid::amqp_1_0::client::AcknowledgeMode::ALO, "eventhubs-receiver-link-" + partition.to_s, false, filters, nil)
         receiver.setCredit(org::apache::qpid::amqp_1_0::type::UnsignedInteger.valueOf(@receive_credits), true)
-        process(output_queue,receiver,partition)
+        last_event_offset = process(output_queue,receiver,partition, last_event_offset)
       rescue org::apache::qpid::amqp_1_0::client::ConnectionErrorException => e
-        @logger.debug("  " + partition.to_s.rjust(2,"0") + " --- " + "resetting connection")
-        @time_since_epoch_millis = Time.now.utc.to_i * 1000
+        sleep(@thread_wait_sec)
+        @logger.debug("[#{partition.to_s.rjust(2,"0")}] resetting connection")
+        receiver.close()
       end
     end
   rescue LogStash::ShutdownSignal => e
     receiver.close()
     raise e
   rescue => e
-    @logger.error("  " + partition.to_s.rjust(2,"0") + " --- Oh My, An error occurred.", :exception => e)
-  end # process
+    @logger.error("[#{partition.to_s.rjust(2,"0")}] Oh My, An error occurred. \nError:#{e}:\nTrace:\n#{e.backtrace}", :exception => e)
+  end # process_partition
 
   public
   def run(output_queue)
